3d584f9d4a4f92071d8633dcb5488dc05995b871
guilhermelima18 <guilhermelima18@hotmail.com>
2021-09-30 00:04:34 -0300

    Criando componente de paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useState } from "react";
 import {
   Box,
   Flex,
@@ -15,13 +15,16 @@ import {
   Text,
   useBreakpointValue,
   Spinner,
+  Link,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination/index";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
-import Link from "next/link";
-import { useQuery } from "react-query";
+import NextLink from "next/link";
+import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
@@ -29,31 +32,23 @@ export default function UserList() {
     lg: true,
   });
 
-  const { data, isLoading, error } = useQuery(
-    "users",
-    async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+  const [page, setPage] = useState(1);
 
-      const users = data.users.map(({ id, name, email, createdAt }) => {
-        return {
-          id,
-          name,
-          email,
-          createdAt: new Date(createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    }
-  );
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(
+      ["user", userId],
+      async () => {
+        const response = await api.get(`users/${userId}`);
+
+        return response.data;
+      },
+      {
+        staleTime: 1000 * 60 * 10, // 10 minutes
+      }
+    );
+  }
 
   return (
     <Box>
@@ -64,8 +59,11 @@ export default function UserList() {
           <Flex mb="8" justifyContent="space-between" alignItems="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -75,7 +73,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
           {isLoading ? (
             <Flex justifyContent="center">
@@ -99,14 +97,16 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(({ id, name, email, createdAt }) => (
+                  {data.users.map(({ id, name, email, createdAt }) => (
                     <Tr key={id}>
                       <Td px={["4", "4", "6"]}>
                         <Checkbox colorScheme="pink" />
                       </Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold">{name}</Text>
+                          <Link onMouseEnter={() => handlePrefetchUser(id)}>
+                            <Text fontWeight="bold">{name}</Text>
+                          </Link>
                           <Text fontSize="sm" color="gray.300">
                             {email}
                           </Text>
@@ -128,7 +128,11 @@ export default function UserList() {
                   ))}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,16 +1,16 @@
-import { createServer, Factory, Model } from 'miragejs'
-import faker from 'faker'
+import { createServer, Factory, Model, Response } from "miragejs";
+import faker from "faker";
 
 type User = {
   name: string;
   email: string;
   created_at: string;
-}
+};
 
 export function makeServer() {
   const server = createServer({
     models: {
-      user: Model.extend<Partial<User>>({})
+      user: Model.extend<Partial<User>>({}),
     },
 
     factories: {
@@ -24,24 +24,40 @@ export function makeServer() {
         createdAt() {
           return faker.date.recent(10, new Date());
         },
-      })
+      }),
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList("user", 200);
     },
 
     routes() {
-      this.namespace = 'api';
+      this.namespace = "api";
       this.timing = 750;
 
-      this.get('/users');
-      this.post('/users');
+      this.get("/users", async function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
 
-      this.namespace = '';
+        const total = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
+
+      this.get("/users/:id");
+      this.post("/users");
+
+      this.namespace = "";
       this.passthrough();
-    }
-  })
+    },
+  });
 
   return server;
 }
